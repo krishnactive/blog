916c830eaf3d79c3360ad0d30fa50cfda8bcbf51
krishna <krishnactive@gmail.com>
2024-09-19 16:33:11 +0530

    React Router refer notes

--- src/App.js
+++ src/App.js
@@ -1,51 +1,24 @@
-
-// import './App.css';
-//this div is injected into the html file
-//app.js this called as root component of application meaning ie first component that is going to be rendered to the DOM Index file render it right there
-//  function App() {
-  
-//   const title = "Welcome to the new blog";
-//   const likes = 50 ;
-//   // const person = {name:'Yoshi', age: 30};
-//   const link = "http://www.google.com"
-//   const handleClick = (event,url)=>{
-//     event.preventDefault(); //prevent page reload
-//     window.open(url,"_blank"); //prevent the current page from reload but open the passed url in new tab
-//     console.log ("relaod prevented");
-//   };
-//   return (
-//     <div className="App">
-//       <div className="content">
-//         <h1>{title}</h1>
-//         <p>Liked {likes} times</p>
-//         {/* <p>{person}</p> */}
-//         <p>{[1,2,3,4]}</p>
-//         <p>{"krishna kk"}</p>
-//         <p>{Math.random()*10}</p>
-//         <a href={link} onClick={(event)=>handleClick(event,link)}>Google site</a>
-//       </div>
-//     </div>
-//   );
-// }
-
-//for prventing page reload or any thing u can use functions that prevent it in attributes u can add function to preform any task if required
-//dynamuics value for any attribute ... just use curly brackets{}
-//curly brackets for dynamics value input for
-
-// now navbar.js must be imported here anywhere in the app function which is passed to index.js file which is going to be renderd in html.js
-
-import Navbar from './Navbar'; //selfclosing in or it can be other type also
+import Navbar from './Navbar';
 import Home from './Home';
-
+import { BrowserRouter as Router ,Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <div className="content">
-        <Home/>
-      </div>
-    </div>
+    <Router>
+        <div className="App">
+          <Navbar/>
+          <div className="content">
+          <Switch>
+                  <Route path="/home">
+                      <Home/>
+                  </Route>
+              </Switch>
+          </div>
+        </div>
+    </Router>
+    
   );
 }
 export default App;
+// switch - route are component of react-router-dom
+//switch -> route-path make the element available if that path is visited otherwise the current path element is shown
--- src/Home.js
+++ src/Home.js
@@ -3,8 +3,8 @@ import BlogList from "./BlogList";
 import useFetch from './useFetch'
 
 const Home = () => {
-    const {data:blogs, isPending, error}  = useFetch('http://localhost:8000/blogs') //naming other name use : after actual name
- // function can be used multiple time and it also become neater and tidier as well and they're much eaiser to read
+    const {data:blogs, isPending, error}  = useFetch('http://localhost:8000/blogs');
+ 
     return ( 
         <div className="Home">
             {error && <div>{error}</div>}
@@ -15,13 +15,3 @@ const Home = () => {
     );
 }    
 export default Home;
-
-// we need to do things in another component in future where we fetch some data
-    // we create state for the data itself essential we have write all those code of error isPending
-    //reuseability to have this
-    // custom hooks 
-    // In React, a custom hook is a JavaScript function that allows you to reuse 
-    // stateful logic across multiple components. Custom hooks enable you to 
-    // encapsulate and share common logic that can involve using React’s 
-    // built-in hooks (like useState, useEffect, useContext, etc.), so you don’t 
-    // have to repeat yourself (DRY principle) in different components.
