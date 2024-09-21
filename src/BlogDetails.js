5eecc397f66e88e5a16fb3334c7e83afb3f50aa2
krishna <krishnactive@gmail.com>
2024-09-21 12:42:44 +0530

    Reusing custom hooks ie using using that useFetch function multiple time

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,20 @@
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
-//useParams : allows us to grab parameters or route parameters fromt the route
-// need to destructure whichever route parameters we want 
+import useFetch from "./useFetch";
+
 const BlogDetails = () => {
     const { id } = useParams();
-
+    const {data:blog, error, isPending} = useFetch('http://localhost:8000/blogs/'+id);
     return ( 
         <div className="blog-details">
-            <h2>Blogs Details - { id }</h2>
+            {isPending && <div>Loading...</div>}
+            {error && <div>{ error }</div>}
+            { blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+            )}
         </div>
     );
 }
