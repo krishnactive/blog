e3b83ca9e1a3f94af8f582f1cca4cafb69a73233
krishna <krishnactive@gmail.com>
2024-09-21 11:57:00 +0530

    useEffect Cleanup

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,5 +1,7 @@
 import {useState, useEffect} from 'react';
 
+// making a cleanup function to stop the change router to run in background or to stop it 
+// 
 const useFetch = (url)=>{
 
     const [data, setData] = useState(null);
@@ -8,8 +10,10 @@ const useFetch = (url)=>{
 
 
     useEffect(()=>{
+        const abortCont = new AbortController();
+
         setTimeout(() => {
-         fetch(url)
+         fetch(url,{signal: abortCont.signal})
              .then(res => {
                  if(!res.ok){
                      throw Error('could not fetch the data for that resource');
@@ -22,13 +26,25 @@ const useFetch = (url)=>{
                  setError(null);
              })
              .catch( err =>{
-                 setIsPending(false);
-                 setError(err.message);
+                if(err.name ==='AbortError'){
+                    console.log('Fetch aborted')
+                }
+                else{
+                    setIsPending(false);
+                    setError(err.message);
+                }
+                 
              }) 
         }, 1000);
+       return () => abortCont.abort();
      }, [url]); // this dependency if whenever url is gonna changes its going to re-run this function to get the data for that endpoint 
 
      return {data, isPending, error}
 }
 
 export default useFetch
+
+// in abort error error we not want to update the state 
+// component trying to update and we won't get that error 
+// error anymore 
+// use of cleanfunction and abort controller 
