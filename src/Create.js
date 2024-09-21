15e577b8ffd1a7d3ece967d60b9435fe7ebaa9ce
krishna <krishnactive@gmail.com>
2024-09-21 11:05:04 +0530

    Exact Match Routes

---
+++ src/Create.js
@@ -0,0 +1,12 @@
+const Create = () => {
+    return ( 
+        <div>
+            <h2>Add a new Blog</h2>
+        </div>
+     );
+}
+ 
+<div>
+    <h2>Add a new </h2>
+</div>
+export default Create;
